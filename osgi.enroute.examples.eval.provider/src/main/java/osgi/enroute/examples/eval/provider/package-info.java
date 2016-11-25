fc05137c3eb1002e33e7e08c4f47e41c6088625b
syedshahul <syedshahul2002@gmail.com>
2016-11-25 21:12:00 +0530

    enRoute example

---
+++ osgi.enroute.examples.eval.provider/src/main/java/osgi/enroute/examples/eval/provider/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author osgi
+ *
+ */
+package osgi.enroute.examples.eval.provider;
---
+++ osgi.enroute.examples.eval.provider/target/classes/OSGI-OPT/src/osgi/enroute/examples/eval/api/Eval.java
@@ -0,0 +1,12 @@
+package osgi.enroute.examples.eval.api;
+
+/**
+ * 
+ */
+public interface Eval {
+	
+	/**
+	 * 
+	 */
+	double eval(String expression) throws Exception;
+}
